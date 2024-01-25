7a371d2bb9000c557fd747afa53f95cfeae9d3a8
jose noboa <j.noboa@outlook.com>
2024-01-25 17:06:44 -0500

    updated

---
+++ practice.rb
@@ -0,0 +1,52 @@
+#name = "billy"
+#if name == "jose"
+  #  puts "hello, jose"
+#else 
+  #  puts "nice to meet you, #{name} "
+#end
+
+#password = "joseluis#"
+#number_char = password.length
+
+##      puts "got it, your password is, #{password}"
+#end
+
+#Write a Ruby program that defines a variable that stores an Integer. The program should print out the String "even" if the Integer is even, #and the String "odd" if the Integer is odd.
+
+
+
+#number = 7
+#if number.odd?
+   # puts "odd"
+#else puts "even"
+#end
+
+#Write a Ruby program that prints out a String or Integer: The printed value will depend on the value of an Integer. If the Integer is a #multiple of 3, print "Fizz". If the Integer is a multiple of 5, print "Buzz". If the Integer is a multiple of both 3 and 5, print #"FizzBuzz". If the Integer is not a multiple of either, print the Integer itself.#
+
+#number = 15
+ #   if number % 3 == 0 && number % 5 == 0
+  ## elsif number % 3 == 0 
+    #    puts "fizz"
+    ##   puts "buzz"
+    #else puts "invalid"
+    #end
+
+
+    #def get_age(num1)
+    #    2024 - num1
+    #end
+    #persons_age = get_age(1976)
+    #puts " you have lived #{persons_age}  years of life"
+    
+
+
+
+
+
+
+
+
+
+
+
+
